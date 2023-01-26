8eaa8f8b29fb9f75f6f459465873029ec582c9b3
Daniel King <daniel@mosaicml.com>
2023-01-25 18:49:51 -0800

    debug print

--- deepspeed/launcher/multinode_runner.py
+++ deepspeed/launcher/multinode_runner.py
@@ -269,6 +269,7 @@ class MosaicMLRunner(MultiNodeRunner):
                     if 'config_files' in arg_dict:
                         config_files = {}
                         for k, v in arg_dict.get('config_files', {}).items():
+                            print('+'*30, v)
                             config_files[k] = json.loads(v)
                         arg_dict['config_files'] = config_files
                 except json.JSONDecodeError as jde:
